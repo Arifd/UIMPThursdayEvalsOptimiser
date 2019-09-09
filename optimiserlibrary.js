4bce12c74ac8ed24d973be70cc911e6c239c0101
Arif Driessen <arifd86@gmail.com>
2019-09-09 23:13:59 +0200

    Update optimiserlibrary.js

--- optimiserlibrary.js
+++ optimiserlibrary.js
@@ -9,7 +9,7 @@ function appendRedundancy()
 	var today = new Date();
 	if (today.getDay() == 4) { var theFriday = new Date(today.getTime() + (24 * 60 * 60 * 1000)); alert("shit"); }
 	else if (today.getDay() == 5) { var theFriday = today; alert("fuck"); }
-	else { var theFriday = "ERROR"; alert("bong"); }
+	else { var theFriday = "ERROR"; alert(theFriday); }
  	//document.getElementById("formDate").value = d.toDateString();
  	document.getElementById("formMonth").value = theFriday.getMonth()+1;
  	document.getElementById("formDay").value = theFriday.getDate();
