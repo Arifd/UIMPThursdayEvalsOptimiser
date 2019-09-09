dbb83b0b82ece21bc3bbd0f760723b6faf735dd3
Arif Driessen <arifd86@gmail.com>
2019-09-09 23:12:39 +0200

    Update optimiserlibrary.js

--- optimiserlibrary.js
+++ optimiserlibrary.js
@@ -7,13 +7,14 @@ function appendRedundancy()
 	document.getElementById("processedFirstName").value = splitName[1];
 	// Process date
 	var today = new Date();
-	if (today.getDay() == 4) var theFriday = new Date(today.getTime() + (24 * 60 * 60 * 1000));
-	else if (today.getDay() == 5) var theFriday = today;
-	else var theFriday = "ERROR";
+	if (today.getDay() == 4) { var theFriday = new Date(today.getTime() + (24 * 60 * 60 * 1000)); alert("shit"); }
+	else if (today.getDay() == 5) { var theFriday = today; alert("fuck"); }
+	else { var theFriday = "ERROR"; alert("bong"); }
  	//document.getElementById("formDate").value = d.toDateString();
  	document.getElementById("formMonth").value = theFriday.getMonth()+1;
  	document.getElementById("formDay").value = theFriday.getDate();
  	document.getElementById("formYear").value = theFriday.getFullYear();
+	alert("done");
 	function getWeekNumber(d)
 	{
 	    // Copy date so don't modify original
