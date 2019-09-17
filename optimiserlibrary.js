ea7549d5f45756560ad23db629917d32df6f8dab
Arif Driessen <arifd86@gmail.com>
2019-09-17 19:20:41 +0200

    Update optimiserlibrary.js

--- optimiserlibrary.js
+++ optimiserlibrary.js
@@ -36,7 +36,7 @@ function appendRedundancy()
 	begLvl = parseFloat(document.getElementById("beginningLevel").value);
 	endLvl = parseFloat(document.getElementById("endLevel").value);
 	// turn "endLevel" into a string for submitting to UIMP as "Final Score"
-	// document.getElementById("endLevel").value = endLvl.toString();
+	document.getElementById("endLevel").value = endLvl.toString();
 	document.getElementById("comment").value = endLvl;
 	var begCEFRLvl = ""; // A1, A2, B1, B2, C1, C2
 	var endCEFRLvl = ""; // A1, A2, B1, B2, C1, C2
