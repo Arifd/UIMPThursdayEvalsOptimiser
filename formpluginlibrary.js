1dcd92e8bb29dfc17581a55c47cfb55de9e56165
Arif Driessen <arifd86@gmail.com>
2019-09-23 16:28:45 +0200

    Update formpluginlibrary.js

--- formpluginlibrary.js
+++ formpluginlibrary.js
@@ -77,10 +77,10 @@ function pluginDummyForm()
 	document.getElementById("changedLevel").name = "entry.502099535";
 
 	for (i = 1; i < 6; i++) {document.getElementById("fluency-"+i).name = "entry.1839803024";}
-	document.getElementById("pronunciation").name = "entry.498561152";
-	document.getElementById("grammar").name = "entry.1379348697";
-	document.getElementById("vocabulary").name = "entry.835827768";
-	document.getElementById("comprehension").name = "entry.1952347568";
+	for (i = 1; i < 6; i++) {document.getElementById("pronunciation-"+i).name = "entry.498561152";
+	for (i = 1; i < 6; i++) {document.getElementById("grammar-"+i).name = "entry.1379348697";
+	for (i = 1; i < 6; i++) {document.getElementById("vocabulary-"+i).name = "entry.835827768";
+	for (i = 1; i < 6; i++) {document.getElementById("comprehension-"+i).name = "entry.1952347568";
 
 	document.getElementById("formHomework").name = "entry.2054164790";
 	document.getElementById("formParticipation").name = "entry.230056582";
