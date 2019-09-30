333ef43b459e1e34d73f20693cca5ee3cc94c368
Arif Driessen <arifd86@gmail.com>
2019-09-30 07:06:34 +0200

    Update optimiserlibrary.js

--- optimiserlibrary.js
+++ optimiserlibrary.js
@@ -2,8 +2,8 @@
 function applyClass(className, element, hide_after = false)
 {
   // find out if element has been passed as the name or the object
-  if (typeof element == string) element = document.getElementById(element);
-  if (typeof element != (string || object)) console.log("applyClass: element not an object nor a string");
+  if (typeof element == 'string') element = document.getElementById(element);
+  if (typeof element != ('string' || 'object')) console.log("applyClass: element not an object nor a string");
   if (element.classList.contains(className))
   {
     element.classList.remove(className);
