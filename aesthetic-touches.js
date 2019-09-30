9d25c703a33675366c29223fa5532a11f7a98299
Arif Driessen <arifd86@gmail.com>
2019-09-30 06:37:51 +0200

    Create aesthetic-touches.js

---
+++ aesthetic-touches.js
@@ -0,0 +1,18 @@
+// these functions accompany aesthetic-touches.css
+// written and produced by Arif Driessen
+
+// apply a class to an element, with error checking and option to hide the element after. 
+function applyClass(className, element, hide-after = false)
+{
+  if (element.classList.contains(className))
+  {
+    element.classList.remove(className);
+    void element.offsetWidth; // Trigger reflow before re-adding
+    element.classList.add(className);
+    // wait for animation to end then remove the element.
+    if (hide-after) element.addEventListener('animationend', function() { element.style.display = 'none'; }, {once: true});
+  }
+  else element.classList.add(className);
+  // wait for animation to end then remove the element.
+  if (hide-after) element.addEventListener('animationend', function() { element.style.display = 'none'; }, {once: true});
+}
