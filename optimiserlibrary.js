01b77d6acac51435ba7bd34ea7bae633d615e98f
Arif Driessen <arifd86@gmail.com>
2019-10-01 22:05:04 +0200

    Update optimiserlibrary.js

--- optimiserlibrary.js
+++ optimiserlibrary.js
@@ -81,7 +81,7 @@ function adjustSkills()
 	var endLvl = parseFloat(document.getElementById("formEndLevel").value);
 	
 	// if one of the inputs is 0.0 or NaN the user either hasn't entered anything or something has gone wrong, exit and don't do anything!
-	if ((begLvl || endLvl) == (0.0 || NaN)) {alert("yep"); return;} alert("nope");
+	if ((begLvl || endLvl) == (0.0 || NaN)) {alert("yep"); return;} console.log(begLevl); console.log(endLvl);
 	
 	// Send what we recieve from the form to UIMP as "Final Score"
 	document.getElementById("endLevel").value = endLvl.toString();
