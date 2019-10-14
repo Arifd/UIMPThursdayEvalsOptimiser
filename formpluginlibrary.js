8ac4cfb1b6353dd8d9b54035e751246c488251ea
Arif Driessen <arifd86@gmail.com>
2019-10-14 19:14:34 +0200

    Update formpluginlibrary.js

--- formpluginlibrary.js
+++ formpluginlibrary.js
@@ -135,3 +135,50 @@ function pluginSevilla()
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
+
+function pluginCartagena()
+{
+	document.getElementById("theForm").action = "https://docs.google.com/forms/d/e/1FAIpQLSdhE8FGuUxVZ_2-CTmf1OIcjg5MOZBxXCXeqRc47lZVl5z-rQ/formResponse";
+
+	document.getElementById("lastName").name = "entry.539992332";
+	document.getElementById("firstName").name = "entry.1885495846";
+
+	document.getElementById("location").name = "entry.1609186721";
+	// you need to manually fill in the location field here
+	document.getElementById("location").value = "Cartagena";
+
+	document.getElementById("formMonth").name = "entry.905372383_month";
+	document.getElementById("formDay").name = "entry.905372383_day";
+	document.getElementById("formYear").name = "entry.905372383_year";
+
+	document.getElementById("passOrFail").name = "entry.420853720";
+
+	document.getElementById("processedISOWeekNumber").name = "entry.1784072739";
+
+	document.getElementById("group").name = "entry.1771980111";
+
+	document.getElementById("teachersNames").name = "entry.42228523";
+
+	// CEFR Level at Beginning
+	document.getElementById("levelAtBeginning").name = "entry.86321645";
+	// CEFR Level at End
+	document.getElementById("levelAtEnd").name = "entry.605540039";
+	// final score
+	document.getElementById("endLevel").name = "entry.642928071";
+
+	document.getElementById("changedLevel").name = "entry.1898877679";
+
+	for (let i = 1; i < 6; i++) {document.getElementById("fluency-"+i).name = "entry.916825764";}
+	for (let i = 1; i < 6; i++) {document.getElementById("pronunciation-"+i).name = "entry.1764100518";}
+	for (let i = 1; i < 6; i++) {document.getElementById("grammar-"+i).name = "entry.2128361282";}
+	for (let i = 1; i < 6; i++) {document.getElementById("vocabulary-"+i).name = "entry.978742957";}
+	for (let i = 1; i < 6; i++) {document.getElementById("comprehension-"+i).name = "entry.24510025";}
+
+	document.getElementById("formHomework").name = "entry.1208927224";
+	document.getElementById("formParticipation").name = "entry.642674893";
+
+	document.getElementById("comment").name = "entry.1977146748";
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
